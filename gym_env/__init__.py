172771a7b189f551966e0b05c24cc1b18a1020d6
Dong Ki Kim <dkkim93@mit.edu>
2019-08-08 16:21:57 -0400

    Minor. Added n_agent in gymenv as input

--- gym_env/__init__.py
+++ gym_env/__init__.py
@@ -6,6 +6,6 @@ from gym.envs.registration import register
 register(
     id='Gridworld-v0',
     entry_point='gym_env.gridworld.gridworld_env:GridworldEnv',
-    kwargs={'row': 1},
+    kwargs={'row': 1, 'n_agent': 2},
     max_episode_steps=10
 )

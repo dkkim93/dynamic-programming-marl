b23e1bb67ec0e83472c1a160ddba5fdc0bdff9bc
Dong Ki Kim <dkkim93@mit.edu>
2019-08-12 14:15:42 -0400

    Improved visualization

--- misc/vis.py
+++ misc/vis.py
@@ -1,13 +1,17 @@
-import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+import pylab as plot
+
+sns.set_style("whitegrid")
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+params = {'legend.fontsize': 14}
+plot.rcParams.update(params)
 
 
 def vis(table):
     fig, ax = plt.subplots()
 
-    ax.matshow(table, cmap='seismic')
-
-    for (i, j), z in np.ndenumerate(table):
-        ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
-
-    plt.show()
+    sns.heatmap(table, annot=True, fmt=".2f", cmap="YlGnBu", center=0.7)
+    plt.xlabel(r'\textbf{Agent $2$ Location}', size=16)
+    plt.ylabel(r'\textbf{Agent $1$ Location}', size=16)
